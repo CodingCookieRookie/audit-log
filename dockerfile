035b360c678b9caed2fa6e9e626e74c262962188
Alvin <bioluminescencecloud@gmail.com>
2023-06-03 00:15:05 +0800

    remove comments and unused code

--- dockerfile
+++ dockerfile
@@ -1,13 +1,3 @@
-# FROM ubuntu:20.04
-# RUN \
-#   apt-get update && \
-#   DEBIAN_FRONTEND=noninteractive apt install -y mysql-server && \
-#   service mysql start
-# WORKDIR /app
-# COPY audit-log .
-# EXPOSE 3306
-# CMD ["service mysql start && ./audit-log"]
-
 FROM ubuntu:20.04
 
 WORKDIR /app
@@ -16,10 +6,8 @@ COPY audit-log /app/
 
 COPY .env .
 
-# Install necessary packages
 RUN \
     apt-get update && \
-    # apt-get install -y supervisor && \
     apt-get install -y ca-certificates && update-ca-certificates
 
 CMD ["./audit-log", "-n"]
--- handlers/gin.go
+++ handlers/gin.go
@@ -1,35 +1,10 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
-	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 )
-
-func NewStackTracedError(err error) *StackTracedError {
-	return &StackTracedError{
-		err:   err,
-		stack: debug.Stack(),
-	}
-}
-
-func NewStackTracedErrorf(format string, args ...interface{}) *StackTracedError {
-	return &StackTracedError{
-		err:   fmt.Errorf(format, args...),
-		stack: debug.Stack(),
-	}
-}
-
-type StackTracedError struct {
-	err   error
-	stack []byte
-}
-
-func (ste *StackTracedError) Error() string {
-	return ste.err.Error() + "\n" + string(ste.stack)
-}
 
 type ErrorMessage struct {
 	Error string `json:"error"`
