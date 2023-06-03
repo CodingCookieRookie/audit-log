8e89c1b4b971e062f41515df09332690c5e571c0
Alvin <bioluminescencecloud@gmail.com>
2023-06-03 15:07:07 +0800

    edit docker-start.sh

--- docker-start.sh
+++ docker-start.sh
@@ -2,12 +2,14 @@ sudo apt-get update
 
 sudo apt install docker.io -y
 
-sudo dockerd
+sudo dockerd &
+
+sleep 3
 
 # sudo docker kill $(sudo docker ps -a -q) &   # Switch this to docker kill container ID of audit-log if there are other existing docker containers
 
 # sudo docker rm $(sudo docker ps -a -q) &     # Switch this to docker rm container ID of audit-log if there are other existing docker containers 
 
-docker pull codingcookierookie/audit-log &
+docker pull codingcookierookie/audit-log
 
-sudo docker run -p 3000:3000 -d codingcookierookie/audit-log &
+sudo docker run -p 3000:3000 -d codingcookierookie/audit-log
